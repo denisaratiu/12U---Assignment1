bc928be0047eb5a3d03538fbcb82b3e99df953c2
ratid6445 <ratid6445@HRH-IC0027347.wrdsb.ca>
2017-09-13 09:51:55 -0400

    Review - finished questions 1-8, question 9 is in progress

--- src/javareviewquestions/denisa/A1Q8.java
+++ src/javareviewquestions/denisa/A1Q8.java
@@ -16,17 +16,17 @@ public class A1Q8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // create a scanner to get user to input a number
+        // create a scanner to get user input
         Scanner input = new Scanner(System.in);
         // ask the user input the initial amount
         System.out.println("Please input the current amount of money in your savings account");
         // scan in the number that the user inputs
         // use a double due to decimal numbers (cents)
-        double initialAmount = input.nextInt();
+        double initialAmount = input.nextDouble();
         // ask the user to input the interest rate
         System.out.println("Please input the interest rate as a decimal");
         // allow user to input a number that represents the interest rate
-        double interestRate = input.nextInt();
+        double interestRate = input.nextDouble();
 
         // create two integers that stores the number of years to double and get to 1 million dollars
         int doublingTime = 0;
@@ -39,12 +39,14 @@ public class A1Q8 {
         }
         // calculate the amount of time needed to reach 1 million dollars
         for (double millionBalance = initialAmount; millionBalance < 1000000;
-                millionBalance = millionBalance + interestRate) {
+                millionBalance = (1 + interestRate) * millionBalance) {
             // add one year each time to the time it will take to reach 1 million dollars
             millionDollarTime++;
         }
         // output the answers
-        System.out.println(doublingTime);
-        System.out.println(millionDollarTime);
+        System.out.println("It will take " + doublingTime + " year(s) for the money in the "
+                + "savings account to double at the specified rate.");
+        System.out.println("It will take " + millionDollarTime + " year(s) "
+                + "to reach 1 million dollars.");
     }
 }
---
+++ src/javareviewquestions/denisa/A1Q9.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javareviewquestions.denisa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ratid6445
+ */
+public class A1Q9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner to get user to input words
+        Scanner input = new Scanner(System.in);
+        // ask user to input 10 words
+        System.out.println("Enter 10 words below: ");
+        // create a loop that allows user to input 10 words
+        for( int i = 0; i < 10; i++){
+            // get user input
+            String words = input.nextLine();
+            int size = words.length();
+        }
+        
+    }
+}
