7aad344d15fddd840186c1a48901a948652bc8c5
ratid6445 <ratid6445@HRH-IC0027347.wrdsb.ca>
2017-09-15 09:18:44 -0400

    Review - finished all questions

--- src/javareviewquestions/denisa/A1Q6.java
+++ src/javareviewquestions/denisa/A1Q6.java
@@ -22,11 +22,17 @@ public class A1Q6 {
         System.out.println("Enter a number between 50 and 100 to stop at:");
         // scan the users input 
         int stopCount = input.nextInt();
+        // make sure the number inputted was in between 50 and 100
+        if (stopCount <= 50 || stopCount >= 100) {
+            System.out.println("Sorry, the number inputted was not inbetween 50 and 100.");
+        }
         // start count from 100, stop when the counter gets to the number inputted
         // go down by 5
-        for (int i = 100; i > stopCount; i = i - 5) {
-            // output the numbers counted backwards from 100 by 5's until stopped
-            System.out.println(i);
+        if (stopCount >= 50 && stopCount <= 100) {
+            for (int i = 100; i > stopCount; i = i - 5) {
+                // output the numbers counted backwards from 100 by 5's until stopped
+                System.out.println(i);
+            }
         }
     }
 }
